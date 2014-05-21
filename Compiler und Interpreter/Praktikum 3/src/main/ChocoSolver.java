facbc79842e90ce0241e433a70fbff449572b833
Artanoah <artanoah@web.de>
2014-05-21 16:04:10 +0200

    CIPR3

--- Compiler und Interpreter/Praktikum 3/src/main/ChocoSolver.java
+++ Compiler und Interpreter/Praktikum 3/src/main/ChocoSolver.java
@@ -1,10 +1,13 @@
 package main;
 
 import choco.Choco;
+import choco.Options;
 import choco.cp.model.CPModel;
+import choco.cp.solver.CPSolver;
 import choco.kernel.model.Model;
 import choco.kernel.model.constraints.Constraint;
 import choco.kernel.model.variables.integer.IntegerVariable;
+import choco.kernel.solver.Solver;
 
 
 public class ChocoSolver {
@@ -13,60 +16,19 @@ public class ChocoSolver {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int n = 3;
-		int M = n * (n * n + 1) / 2;
-		
 		Model m = new CPModel();
 		
-		IntegerVariable [][] cells = new IntegerVariable[n][n];
-		for(int i = 0; i < n; i++) {
-			for(int j = 0; j < n; j++) {
-				cells[i][j] = Choco.makeIntVar("cell" + j, 1, n*n);
-				m.addVariables(cells[i][j]);
-			}
-		}
+		IntegerVariable d = Choco.makeIntVar("d", 0, 9);
+		IntegerVariable o = Choco.makeIntVar("o", 0, 9);
+		IntegerVariable n = Choco.makeIntVar("n", 0, 9);
+		IntegerVariable a = Choco.makeIntVar("a", 0, 9);
+		IntegerVariable l = Choco.makeIntVar("l", 0, 9);
+		IntegerVariable g = Choco.makeIntVar("g", 0, 9);
+		IntegerVariable e = Choco.makeIntVar("e", 0, 9);
+		IntegerVariable r = Choco.makeIntVar("r", 0, 9);
+		IntegerVariable b = Choco.makeIntVar("b", 0, 9);
+		IntegerVariable t = Choco.makeIntVar("t", 0, 9);
 		
-		Constraint[] rows = new Constraint[n];
-		for(int i = 0; i < n; i++) {
-			rows[i] = Choco.eq(Choco.sum(cells[i]), M);
-		}
-		
-		m.addConstraints(rows);
-		
-		IntegerVariable[][] cellsDual = new IntegerVariable[n][n];
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-					cellsDual[i][j] = cells[j][i];
-			}
-		}
-		
-		Constraint[] cols = new Constraint[n];
-		for(int i = 0; i < n; i++) {
-			cols[i] = Choco.eq(Choco.sum(cellsDual[i]), M);
-		}
-		
-		m.addConstraints(cols);
-		
-		IntegerVariable[][] diags = new IntegerVariable[2][n];
-		for(int i = 0; i < n; i++){
-			diags[0][i] = cells[i][i];
-			diags[1][i] = cells[i][(n-1)-i];
-		}
-		
-		m.addConstraint(Choco.eq(Choco.sum(diags[0]), M));
-		m.addConstraint(Choco.eq(Choco.sum(diags[1]), M));
-		
-		IntegerVariable[] allVars = new IntegerVariable[n*n];
-		for(int i = 0; i < n; i++){
-			for(int j = 0; j < n; j++){
-					allVars[i*n+j] = cells[i][j];
-			}
-		}
-		
-		m.addConstraint(Choco.allDifferent(allVars));
-		
-		
-		Solver s = new CPSolver();
+		m.addConstraint(Choco.eq(Choco.mod(Choco.sum(d, d), 10), t));
 	}
-
 }
